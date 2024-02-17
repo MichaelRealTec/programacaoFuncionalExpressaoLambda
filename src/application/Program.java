bae74e1b86873244c366d9fc7ce05cdfe15f5e34
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-02-17 20:16:00 -0300

    Usando a função Lambda reduzida

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,12 +17,10 @@ public class Program {
 		list.add(new Product("Notebook", 1200.00)); // Adicionando o segundo item
 		list.add(new Product("Tablet", 450.00)); // Adicionando o terceiro item
 		
-		Comparator<Product> comp = (p1, p2) -> {
-				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-			};
+		Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
 		
 		list.sort(comp); // Ordenando a lista
-		
+		 
 		for (Product p : list) {
 			System.out.println(p);
 		}
